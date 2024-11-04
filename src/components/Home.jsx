d8647f2e55a9f2c74bd699b3f5b49f6385b84069
Norman Cheah <cheahhonyuen@gmail.com>
2024-11-04 14:23:37 +0800

    devicon description

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -108,24 +108,90 @@ function Home() {
                                 <div className="programming">
                                     <h4>Skills</h4>
                                     <div className="skills-icons">
-                                        <i className="devicon-git-plain"></i>
-                                        <i className="devicon-github-plain"></i>
-                                        <i className="devicon-html5-plain"></i>
-                                        <i className="devicon-css3-plain"></i>
-                                        <i className="devicon-bulma-plain"></i>
-                                        <i className="devicon-nodejs-plain"></i>
-                                        <i className="devicon-javascript-plain"></i>
-                                        <i className="devicon-typescript-plain"></i>
-                                        <i className="devicon-python-plain"></i>
-                                        <i className="devicon-react-original"></i>
-                                        <i className="devicon-django-plain"></i>
-                                        <i className="devicon-nodejs-plain"></i>
-                                        <i className="devicon-postman-plain"></i>
-                                        <i className="devicon-postgresql-plain"></i>
-                                        <i className="devicon-mongodb-plain"></i>
-                                        <i className="devicon-heroku-plain"></i>
-                                        <i className="devicon-netlify-plain"></i>
-                                        <i className="devicon-visualstudio-plain"></i>
+                                        <section className="icons">
+                                            <i className="devicon-git-plain"></i>
+                                            <p className="icon-text">Git</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-github-plain"></i>
+                                            <p className="icon-text">Github</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-html5-plain"></i>
+                                            <p className="icon-text">HTML</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-css3-plain"></i>
+                                            <p className="icon-text">CSS</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-bulma-plain"></i>
+                                            <p className="icon-text">Bulma</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-javascript-plain"></i>
+                                            <p className="icon-text">JavaScript</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-typescript-plain"></i>
+                                            <p className="icon-text">TypeScript</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-nodejs-plain"></i>
+                                            <p className="icon-text">Node.js</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-python-plain"></i>
+                                            <p className="icon-text">Python</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-react-original"></i>
+                                            <p className="icon-text">React</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-django-plain"></i>
+                                            <p className="icon-text">Django</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-postman-plain"></i>
+                                            <p className="icon-text">Postman</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-npm-original-wordmark"></i>
+                                            <p className="icon-text">NPM</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-express-original"></i>
+                                            <p className="icon-text">Express</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-postgresql-plain"></i>
+                                            <p className="icon-text">PostgreSQL</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-mongodb-plain"></i>
+                                            <p className="icon-text">MongoDB</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-heroku-plain"></i>
+                                            <p className="icon-text">Heroku</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-netlify-plain"></i>
+                                            <p className="icon-text">Netlify</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-visualstudio-plain"></i>
+                                            <p className="icon-text">VS Code</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-trello-plain"></i>
+                                            <p className="icon-text">Trello</p>
+                                        </section>
+                                        <section className="icons">
+                                            <i className="devicon-slack-plain"></i>
+                                            <p className="icon-text">Slack</p>
+                                        </section>
                                     </div>
                                 </div>
 
